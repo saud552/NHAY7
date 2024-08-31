933bfaa05afcc1a4070bac602c00312060300cbd
Abdullah <146959314+abod-19@users.noreply.github.com>
2024-08-31 22:04:56 +0300

    Update config.py

--- config.py
+++ config.py
@@ -33,7 +33,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/abod-19/AM",
+    "https://github.com/abod-19/Q",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 APK = 5140000000
