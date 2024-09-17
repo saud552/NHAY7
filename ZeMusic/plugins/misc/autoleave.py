b3994a56a520de6a3869ac47e06968e4bba80dee
Abdullah <146959314+abod-19@users.noreply.github.com>
2024-09-17 16:39:05 +0300

    Update autoleave.py

--- ZeMusic/plugins/misc/autoleave.py
+++ ZeMusic/plugins/misc/autoleave.py
@@ -10,7 +10,7 @@ from ZeMusic.utils.database import get_client, is_active_chat, is_autoend
 
 
 async def auto_leave():
-    if AUTO_LEAVING_ASSISTANT == str(True):
+    if config.AUTO_LEAVING_ASSISTANT == str(True):
         while not await asyncio.sleep(1500):
             from ZeMusic.core.userbot import assistants
 
