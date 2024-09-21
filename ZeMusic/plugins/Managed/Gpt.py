f356157a75d0cf6e5c72dee9542937aa2bf97daf
Abdullah <146959314+abod-19@users.noreply.github.com>
2024-09-21 06:37:09 +0300

    Update Gpt.py

--- ZeMusic/plugins/Managed/Gpt.py
+++ ZeMusic/plugins/Managed/Gpt.py
@@ -14,7 +14,7 @@ async def chat_gpt(bot, message):
 
         if len(message.command) < 2:
             await message.reply_text(
-                "⟡ استخدم الأمر هكذا :\n\n ⟡ لوي + سؤالك"
+                "⟡ استخدم الأمر هكذا :\n\n ⟡ رون + سؤالك"
             )
         else:
             a = message.text.split(' ', 1)[1]
