ffa13d18068bccfff5c6ee56225bce5d8a32f87b
botkial <basharseed202@gmail.com>
2024-11-22 17:31:22 +0300

    Update config.py

--- config.py
+++ config.py
@@ -7,23 +7,23 @@ from pyrogram import filters
 load_dotenv()
  
 # Get this value from my.telegram.org/apps
-API_ID = int(getenv("API_ID","20036317"))
-API_HASH = getenv("API_HASH","986cb4ba434870a62fe96da3b5f6d411")
+API_ID = int(getenv("API_ID","28094107"))
+API_HASH = getenv("API_HASH","1ff3955d2e53a2f76c1599b09e8f7dc2")
 
 # Get your token from @BotFather on Telegram.
 BOT_TOKEN = getenv("BOT_TOKEN")
-BOT_NAME = getenv("BOT_NAME","لارين")
-
+BOT_NAME = getenv("BOT_NAME","")
+GPT_NAME = getenv("GPT_NAME","")
 # Get your mongo url from cloud.mongodb.com
-MONGO_DB_URI = getenv("MONGO_DB_URI","mongodb+srv://Music:Music@cluster0.yjgit72.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+MONGO_DB_URI = getenv("MONGO_DB_URI","mongodb+srv://proceed58:proceed58@cluster0.p5s9ym5.mongodb.net/?retryWrites=true&w=majority")
 
 DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 480))
 
 # Chat id of a group for logging bot s activities
-LOGGER_ID = int(getenv("LOGGER_ID","-1001756578463"))
+LOGGER_ID = int(getenv("LOGGER_ID"))
 
 # Get this value from @FallenxBot on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", 5145609515))
+OWNER_ID = int(getenv("OWNER_ID", 7540129630))
 
 ## Fill these variables if you re deploying on heroku.
 # Your heroku app name
@@ -33,15 +33,14 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/botkial/iumusic",
+    "https://github.com/botkial/music",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
-APK = 5140000000
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-CHANNEL_NAME = getenv("CHANNEL_NAME", "𝐒𝐨𝐮𝐫𝐜𝐞 𝐋𝐨𝐋🧚‍♀")
+CHANNEL_NAME = getenv("CHANNEL_NAME", "𝐒𝐨𝐮𝐫𝐜𝐞 𝐋𝐨𝐋🧚‍♀ ")
 CHANNEL_LINK = getenv("CHANNEL_LINK", "YMMYn")
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/K55DD")
 
@@ -65,8 +64,7 @@ TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", 1073741824))
 
 
 # Get your pyrogram v2 session from @StringFatherBot on Telegram
-AMK = APK + 5600000
-STRING1 = getenv("STRING_SESSION", None)
+STRING1 = getenv("STRING_SESSION",None)
 STRING2 = getenv("STRING_SESSION2", None)
 STRING3 = getenv("STRING_SESSION3", None)
 STRING4 = getenv("STRING_SESSION4", None)
@@ -79,23 +77,22 @@ lyrical = {}
 votemode = {}
 autoclean = []
 confirmer = {}
-ANK = AMK + 9515
 
 
-START_IMG_URL = getenv("START_IMG_URL","https://te.legra.ph/file/e8bdc13568a49de93b071.jpg")
-PING_IMG_URL = "https://te.legra.ph/file/b8a0c1a00db3e57522b53.jpg"
-PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
-STATS_IMG_URL = "https://te.legra.ph/file/e906c2def5afe8a9b9120.jpg"
-TELEGRAM_AUDIO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
-TELEGRAM_VIDEO_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
-STREAM_IMG_URL = "https://te.legra.ph/file/bd995b032b6bd263e2cc9.jpg"
-SOUNCLOUD_IMG_URL = "https://te.legra.ph/file/bb0ff85f2dd44070ea519.jpg"
-YOUTUBE_IMG_URL = "https://telegra.ph/file/f995c36145125aa44bd37.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://te.legra.ph/file/37d163a2f75e0d3b403d6.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://te.legra.ph/file/b35fd1dfca73b950b1b05.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://te.legra.ph/file/95b3ca7993bbfaf993dcb.jpg"
+START_IMG_URL = "https://telegra.ph/file/45aa2586f2abb846ad87c.jpg"
+PING_IMG_URL = "https://telegra.ph/file/45aa2586f2abb846ad87c.jpg"
+PLAYLIST_IMG_URL = "https://telegra.ph/file/45aa2586f2abb846ad87c.jpg"
+STATS_IMG_URL = "https://telegra.ph/file/45aa2586f2abb846ad87c.jpg"
+TELEGRAM_AUDIO_URL = "https://telegra.ph/file/45aa2586f2abb846ad87c.jpg"
+TELEGRAM_VIDEO_URL = "https://telegra.ph/file/45aa2586f2abb846ad87c.jpg"
+STREAM_IMG_URL = "https://telegra.ph/file/45aa2586f2abb846ad87c.jpg"
+SOUNCLOUD_IMG_URL = "https://telegra.ph/file/45aa2586f2abb846ad87c.jpg"
+YOUTUBE_IMG_URL = "https://telegra.ph/file/e29510f6e263bb5264cfb.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://telegra.ph/file/45aa2586f2abb846ad87c.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://telegra.ph/file/45aa2586f2abb846ad87c.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://telegra.ph/file/45aa2586f2abb846ad87c.jpg"
 
-DAV = ANK
+
 def time_to_seconds(time):
     stringt = str(time)
     return sum(int(x) * 60**i for i, x in enumerate(reversed(stringt.split(":"))))
@@ -104,6 +101,11 @@ def time_to_seconds(time):
 DURATION_LIMIT = int(time_to_seconds(f"{DURATION_LIMIT_MIN}:00"))
 
 
+if CHANNEL_LINK:
+    if not re.match("(?:http|https)://", CHANNEL_LINK):
+        raise SystemExit(
+            "[ERROR] - Your CHANNEL_LINK url is wrong. Please ensure that it starts with https://"
+        )
 
 if SUPPORT_CHAT:
     if not re.match("(?:http|https)://", SUPPORT_CHAT):
