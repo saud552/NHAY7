dff40142790d44f6ca37e2e110f4f4a8543a9ae6
botkial <basharseed202@gmail.com>
2024-11-23 05:35:04 +0300

    Update Ashtrak.py

--- ZeMusic/plugins/play/Ashtrak.py
+++ ZeMusic/plugins/play/Ashtrak.py
@@ -6,30 +6,28 @@ from pyrogram.errors import UserNotParticipant
 from ZeMusic import app
 import config
 
-channel = config.CHANNEL_LINK
+channel = "YMMY8"
+Nem = config.BOT_NAME + " شغل"
 async def subscription(_, __: Client, message: Message):
-    if message.from_user:
-        user_id = message.from_user.id
-        try: 
-            await app.get_chat_member(channel, user_id)
-        except UserNotParticipant: 
-            return False
-        return True
+    user_id = message.from_user.id
+    try: 
+        await app.get_chat_member(channel, user_id)
+    except UserNotParticipant: 
+        return False
     return True
     
 subscribed = filters.create(subscription)
 
 # تعريف دالة لمعالجة الأوامر
-@app.on_message(filters.command(["تشغيل", "شغل"],"") & ~subscribed)
+@app.on_message(filters.command(["تشغيل", "بحث", "تخطي", "استئناف", "تقديم", "تحميل", "توقف", "مؤقت", "كمل", "كملي", "لارين بحث", "غنيلي", "شعر", "قران", "اذكار", "ادعيه", "play", "شغلي", "/start", "vplay", "vتشغيل", "cplay", "cvplay", "playforce", "vplayforce", "cplayforce", "cvplayforce", "start", "stats", "الاوامر", "اوامر", "ميوزك", "بنج", "سرعه", "song", "/song", "شغل",Nem], "") & ~subscribed)
 async def command_handler(_: Client, message: Message):
-    if message.from_user:
-        if message.chat.type in [ChatType.GROUP, ChatType.SUPERGROUP]:
-            user_id = message.from_user.id
-            user = message.from_user.first_name
-            markup = Markup([
-                [Button(config.CHANNEL_NAME, url=f"https://t.me/{channel}")]
-            ])
-            await message.reply(
-                f"⟡ عذرًا عزيزي {user} \n⟡ عليك الاشتراك في قناة البوت أولاً",
-                reply_markup=markup
-            )
+    if message.chat.type in [ChatType.GROUP, ChatType.SUPERGROUP]:
+        user_id = message.from_user.id
+        user = message.from_user.first_name
+        markup = Markup([
+            [Button(text="اضغط للإشتراك", url=f"https://t.me/{channel}")]
+        ])
+        await message.reply(
+            f"<b>↤عذراً عزيزي {user}\n↤عليك الإشتراك في قناة البوت اولاً",
+            reply_markup=markup
+        )
