46ff7d89e3d1d59834323e5ea6f996337c4c4e96
hitlerxoox <khayal2551@gmail.com>
2024-12-26 02:25:04 +0300

    Update config.py

--- config.py
+++ config.py
@@ -33,7 +33,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/botkial/iumusic",
+    "https://github.com/hitlerxoox/NHAY7",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 APK = 5140000000
